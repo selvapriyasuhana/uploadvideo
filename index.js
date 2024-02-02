06b2f3f08efe6f386deffdeacd3cb0d6c2e2d03a
selvapriyasuhana <142295945+selvapriyasuhana@users.noreply.github.com>
2024-02-02 11:40:44 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,49 +1,4 @@
-// var express = require("express");
-// var bodyParser = require("body-parser");
-// var mongoose = require("mongoose");
-// const cors = require("cors");
-// const dotenv = require("dotenv");
-// const multer = require('multer');
-// const AWS = require('aws-sdk');
 
-// var app = new express();
-// const storage = multer.memoryStorage();
-// const upload = multer({ storage: storage });
-
-// var VideoRoutes = require("./Routes/routes.js");
-
-
-// var mongodb = require("./Config/Mongoconfig.js");
-// dotenv.config();
-// app.use(cors());
-// app.use(bodyParser.urlencoded({ extended: true }));
-// app.use(bodyParser.json());
-// app.use(express.json());
-
-// const mongo = mongoose.connect(mongodb.url);
-// mongo.then(
-//   () => {
-//     console.log("Mongo_DB Connected Successfully");
-//   },
-//   (error) => {
-//     console.log(
-//       error,
-//       "Error, While connecting to Mongo_DB somthing went wrong"
-//     );
-//   }
-// );
-
-// var port = process.env.PORT||7000;
-// app.listen(port, () => {
-//   console.log("Server running on port " + port);
-// });
-
-// app.get("/", (req, res) => res.send("s3bucket video "));
-
-// app.use("/api", VideoRoutes);
-
-
-// module.exports = app;
 var express = require("express");
 var bodyParser = require("body-parser");
 var mongoose = require("mongoose");
@@ -60,7 +15,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(express.json());
 
-const mongo = mongoose.connect(mongodb.url);
+const mongo = mongoose.connect(mongodb.url1);
 mongo.then(
   () => {
     console.log("Mongo_DB Connected Successfully");
